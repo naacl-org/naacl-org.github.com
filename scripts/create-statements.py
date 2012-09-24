4664b720008015487381eafcd8153c17757594b7
Anoop Sarkar <anoop@sfu.ca>
2012-09-24 11:40:05 -0700

    fixed error in formatting

--- scripts/create-statements.py
+++ scripts/create-statements.py
@@ -4,10 +4,9 @@ import sys
 def printinfo(istream):
     istream.next() # read header and discard
     for row in istream:
-        print 'h3. "%s %s":%s' % (row[0], row[1], row[5])
-        print "h4. Bio\n%s" % (row[4])
-        print "h4. Candidacy Statement\n%s" % (row[3])
-        print
+        print "h3. \"%s %s\":%s\n" % (row[0], row[1], row[5])
+        print "h4. Bio\n\n", row[4], "\n"
+        print "h4. Candidacy Statement\n\n", row[3], "\n\n"
         #print ', '.join(row)
 
 if __name__ == '__main__':
